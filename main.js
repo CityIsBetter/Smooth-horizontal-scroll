0d4fd597e9ae0cb6df01649fe15c9d2654881ac8
CityIsBetter <123456magimagi@gmail.com>
2023-07-07 11:31:09 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+const track = document.getElementById('track');
+
+window.onmousedown = e => {
+    track.dataset.mouseDownAt = e.clientX;
+}
+window.onmouseup = () => {
+    track.dataset.mouseDownAt = '0';
+    track.dataset.prevPercentage = track.dataset.percentage;
+}
+window.onmousemove = e => {
+    if (track.dataset.mouseDownAt === '0') return;
+
+    const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+        maxDelta = window.innerWidth/1;
+
+    const percentage = (mouseDelta / maxDelta) * -100,
+        nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
+        nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+    track.dataset.percentage = nextPercentage;
+    track.style.transform = `translate(${nextPercentage}%, -50%)`;
+
+    track.animate({
+        transform: `translate(${nextPercentage}%, -50%)`
+      }, { duration: 1200, fill: "forwards" });
+
+    for(const image of track.getElementsByClassName("image")) {
+        image.animate({
+          objectPosition: `${100 + nextPercentage}% center`
+        }, { duration: 1200, fill: "forwards" });
+      }
+}
